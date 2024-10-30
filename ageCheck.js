49207a9eda34d2fa9bd69ff2f13e32cdcb1d5e39
Kaye Janine Remigio <remigiokayejanine@gmail.com>
2024-10-30 20:07:45 +0800

    Add ageCheck.js and index.html

--- ageCheck.js
+++ ageCheck.js
@@ -4,7 +4,7 @@ function askAge(){
     if (age < 13) {
         alert("You are a child.");
     } else if (age >= 13 && age <= 19) {
-        alert("You are an adult.");
+        alert("You are a teenager.");
     } else if (age >= 20) {
         alert("You are an adult.");   
     } else {
